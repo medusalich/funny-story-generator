e155652c3d1e68a39ae490aa9e33c6eed1ec557e
medusalich <stephanie.blestel@gmail.com>
2025-01-19 10:13:22 +0100

    String-Formatierung angewendet

--- src/main.py
+++ src/main.py
@@ -3,27 +3,22 @@
 
 def main():
 
-    print("Herzlich willkommen und viel Spaß", "\n")
+    print("Herzlich willkommen und viel Spaß beim Gestalten deiner Geschichte")
     
-    name = input("Gib deinen Namen ein: ")
-    print("Hallo", name, "\n")
+    name = input("\nGib deinen Namen ein: ")
+    
+    gegenstand = input(f"\nHallo {name}, bitte nenne mir deinen liebsten Gegenstand: ")
+    
+    monat = input("\nGib einen Monat ein: ")
 
-    gegenstand = input("Nenne mir deinen liebsten Gegenstand: ")
-    print("Liebster Gegenstand:", gegenstand, "\n")
-
-    monat = input("Gib einen Monat ein: ")
-    print("Der Monat lautet:", monat, "\n")
-
-    ort = input("Gib einen Ort ein, an dem du jetzt gerne wärst: ")
-    print("Der Ort lautet:", ort, "\n")
-
-    aktivitaet = input("Gib ein, was du jetzt gerne am liebsten tun würdest: ")
-    print("Du würdest jetzt gerne:", aktivitaet, "\n")
-
-    print("Hier folgt deine für dich geschriebene Geschichte:", "\n")
-
-    print("Es war einmal im", monat, "eine Person namens", name, ", die jetzt gern im", ort, "wäre.")
-    print(name, "mag es am liebsten, mit dem", gegenstand, "zu", aktivitaet, ".")
+    ort = input("\nGib einen Ort ein, an dem du jetzt gerne wärst: ")
+    
+    aktivitaet = input("\nGib ein, was du jetzt gerne am liebsten tun würdest: ")
+    
+    print("\nHier folgt deine für dich geschriebene Geschichte:")
+    
+    print(f"\nEs war einmal im {monat} eine Person namens {name}, die jetzt gern im {ort} wäre.")
+    print(f"{name} mag es am liebsten, mit dem {gegenstand} zu {aktivitaet}.")
 
 
 if __name__ == "__main__":
