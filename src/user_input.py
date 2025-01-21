4bfa1a4fb1b0641f0504d91e815a04843aca52b7
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-01-21 20:58:29 +0100

    Modul für User-Abfragen-Verarbeitung

---
+++ src/user_input.py
@@ -0,0 +1,19 @@
+def user_input_ohne_zahl(text: str) -> str:
+    """Frage den Nutzer nach einer Eingabe ohne Zahl""" 
+    while True:
+        worte = input(text)
+        if not any(char.isdigit() for char in worte):
+            return worte
+        else:
+            print("Bitte keine Zahlen verwenden, versuche es nochmal")   
+
+
+def user_input_ohne_buchstabe(text: str) -> int:
+    """Frage den Nutzer nach einer Zahl"""    
+    while True:
+        try:
+            zahl = int(input(text))
+            return zahl
+        except ValueError:
+            print("Bitte eine Zahl eingeben, versuche es nochmal")
+
