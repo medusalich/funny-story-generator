238766140d41c8e13c3c08d836df1aef490127bc
Stephanie Hartwig <stephanie.blestel@gmail.com>
2025-02-09 17:17:10 +0100

    Tests für Funfact
    * MagicMock verwendet um einen festen Wert für datetime.now zu
      erzwingen, damit der Test deterministisch ist.

--- tests/funnystorygeneratortests/test_age.py
+++ tests/funnystorygeneratortests/test_age.py
@@ -1,3 +1,5 @@
+import unittest.mock
+
 import pytest
 
 import src.age
@@ -31,3 +33,47 @@ def test_stufe_alter_ein_unglaubwuerdige_person(alter: int) -> None:
 def test_stufe_alter_ein_noch_nicht_geborene_person(alter: int) -> None:
     alters_einstufung = src.age.stufe_alter_ein(alter)
     assert alters_einstufung == "noch nicht geborene Person"
+
+
+@pytest.mark.parametrize("alter", [125, 120, 116])
+def test_erzeuge_funfact_1900(alter: int) -> None:
+    # Ersetze die Klasse datetime.datetime durch ein MagicMock
+    # MagicMock ist ein Dummy, der es erlaubt, beliebeige Verhaltensweisen für Tests zu simulieren.
+    with unittest.mock.patch('datetime.datetime') as mock_datetime:
+        # Hier wird für datetime.datetime.now().year 2025 als festen Wert vorgegeben.
+        # Dadurch ist dieser Test deterministisch und nicht vom Ausführungszeitpunkt abhängig.
+        mock_datetime.now.return_value.year = 2025
+        funfact = src.age.erzeuge_funfact(alter)
+        assert funfact == "als Autos noch mit Pferden um die Wette fuhren"
+
+
+@pytest.mark.parametrize("alter", [75, 70, 66])
+def test_erzeuge_funfact_1950(alter: int) -> None:
+    with unittest.mock.patch('datetime.datetime') as mock_datetime:
+        mock_datetime.now.return_value.year = 2025
+        funfact = src.age.erzeuge_funfact(alter)
+        assert funfact == "als der Rock'n'Roll die Welt eroberte"
+
+
+@pytest.mark.parametrize("alter", [5, 3, 1, 0])
+def test_erzeuge_funfact_2020(alter: int) -> None:
+    with unittest.mock.patch('datetime.datetime') as mock_datetime:
+        mock_datetime.now.return_value.year = 2025
+        funfact = src.age.erzeuge_funfact(alter)
+        assert funfact == "als „Zoom“ ein neues Synonym für „Treffen“ war"
+
+
+@pytest.mark.parametrize("alter", [126, 210])
+def test_erzeuge_funfact_zu_alt(alter: int) -> None:
+    with unittest.mock.patch('datetime.datetime') as mock_datetime:
+        mock_datetime.now.return_value.year = 2025
+        funfact = src.age.erzeuge_funfact(alter)
+        assert funfact == "als es keine Funfacts gab"
+
+
+@pytest.mark.parametrize("alter", [-1, -5, -21])
+def test_erzeuge_funfact_negativ(alter: int) -> None:
+    with unittest.mock.patch('datetime.datetime') as mock_datetime:
+        mock_datetime.now.return_value.year = 2025
+        funfact = src.age.erzeuge_funfact(alter)
+        assert funfact == "404 ERROR"
